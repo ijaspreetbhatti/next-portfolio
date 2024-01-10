aa561576e9730ee99d9bb0ec7bf7056038d89749
Jay <ijaspreetbhatti@outlook.com>
2024-01-10 14:00:31 -0800

    fix: header font size change

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="w-full bg-theBlue text-white flex flex-col md:flex-row justify-between items-center px-12 py-8 font-heading gap-8">
-            <h1 className="font-bold text-xl">
+            <h1 className="font-bold text-2xl">
                 Jaspreet&nbsp;Singh&nbsp;Bhatti
             </h1>
             <ul className="flex gap-8 items-center">
