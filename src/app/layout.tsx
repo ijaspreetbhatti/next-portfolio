77f8557b0daf042808afab2ca6a45559eb3e5a3e
Jay <ijaspreetbhatti@outlook.com>
2024-01-10 04:27:54 -0800

    chore: updates name to title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Jaspreet Bhatti",
-    description: "Portfolio of Jaspreet Bhatti",
+    title: "Jaspreet Singh Bhatti",
+    description: "Portfolio of Jaspreet Singh Bhatti",
     icons: {
         icon: "/favicon.ico",
     },
